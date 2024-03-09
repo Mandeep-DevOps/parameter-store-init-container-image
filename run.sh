ca3805f769cd6cad141197f35024ac0782a38fa5
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-09 08:29:33 +0530

    Parameter script change

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for param in $PARAMS ; do
   PARAM=$(echo $param | awk -F , '{print $1}')
   KEY=$(echo $param | awk -F , '{print $2}')
-  VALUE=$(aws ssm get-parameter --name $PARAM --region us-east-1 --with-decryption --query "Value" --ouptut text)
+  VALUE=$(aws ssm get-parameter --name $PARAM --region us-east-1 --with-decryption --query "Value" --output text)
   echo "export $KEY=\"$VALUE\"" >>/parameters/params
 done
